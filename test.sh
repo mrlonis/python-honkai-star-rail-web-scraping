44b7a43464f4b486ae11083bc5922ef715ec14a4
Matthew Lonis <mattlonis@gmail.com>
2024-02-11 23:46:16 -0800

    Updating submodule

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 poetry run flake8 src tests scrape_web.py
 poetry run pylint src tests scrape_web.py
-poetry run pydocstyle src tests scrape_web.py
-poetry run bandit -c pyproject.toml -r .
 poetry run pytest --cov --cov-report=html -n auto
 # poetry run python scrape_web.py
