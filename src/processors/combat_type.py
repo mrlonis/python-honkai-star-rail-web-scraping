5927662fcfd7e3134c8c6cf77244daba89d4d59a
Matthew Lonis <mattlonis@gmail.com>
2024-03-13 00:32:42 -0400

    Updating dependencies

--- src/processors/combat_type.py
+++ src/processors/combat_type.py
@@ -1,4 +1,5 @@
 """This module contains the logic for the get_combat_type processor."""
+
 from bs4 import BeautifulSoup, Tag
 
 from src.character_data import CharacterData
@@ -11,6 +12,7 @@ def get_combat_type(soup: BeautifulSoup, character_data: CharacterData):
     if combat_type_result and isinstance(combat_type_result, Tag):
         combat_type = combat_type_result.attrs["alt"].strip()
         print(f"Combat Type: {combat_type}")
+    if combat_type:
         character_data.combat_type_id = combat_type
     else:
         error_message = f"Combat Type not found for {character_data.name}."
